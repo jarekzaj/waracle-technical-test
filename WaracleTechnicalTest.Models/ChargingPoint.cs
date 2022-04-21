6e7b525335f0e49d27914e7176ed4996506d6385
Jaroslaw Zajac <jarekzaj@googlemail.com>
2022-04-21 12:23:57 +0100

    Initial project scaffolding

---
+++ WaracleTechnicalTest.Models/ChargingPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WaracleTechnicalTest.Models
+{
+    public class ChargingPoint
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Comment { get; set; }
+        public string ProtocolVersion { get; set; }
+        public int GroupId { get; set; }
+        public int OwnerId { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+    }
+}
---
+++ WaracleTechnicalTest/Controllers/ChargingPointController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WaracleTechnicalTest.Models;
+
+namespace WaracleTechnicalTest.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChargingPointController : ControllerBase
+    {
+
+        private readonly ILogger<ChargingPointController> _logger;
+
+        public ChargingPointController(ILogger<ChargingPointController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<ChargingPoint> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new ChargingPoint())
+            .ToArray();
+        }
+    }
+}
