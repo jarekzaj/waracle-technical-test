76b329b70e00c377b32a31d403e5649f114de9c4
Jaroslaw Zajac <jarekzaj@googlemail.com>
2022-04-21 18:12:13 +0100

    adding sensitive flag to output

--- Deployment/app-insights.tf
+++ Deployment/app-insights.tf
@@ -10,4 +10,5 @@ resource "azurerm_application_insights" "app_insights" {
 
 output "instrumentation_key" {
   value = azurerm_application_insights.app_insights.instrumentation_key
+  sensitive = true
 }
